16055a7fb0b287fed85e2017d6b8f2e1c3eddaef
root <root@srv1360373.hstgr.cloud>
2026-03-04 14:09:27 +0000

    fix(amqp): provide json converter without forcing RabbitTemplate bean

---
+++ src/main/java/br/com/dms/config/RabbitConfig.java
@@ -0,0 +1,14 @@
+package br.com.dms.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
